1f0527fbff3ca745ae4e8d1bc1ffe62fd62e06af
Antonio Salsi <passy.linux@zresa.it>
2024-01-12 21:58:51 +0100

    feat: add config.h

---
+++ config/hhg-parser-config.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *
+ * Hi Happy Garden
+ * Copyright (C) 2023/2024 Antonio Salsi <passy.linux@zresa.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+
+#ifndef HHG_PARSER_CONFIG_H
+#define HHG_PARSER_CONFIG_H
+
+#define HHG_PARSER_NAME "hi-happy-garden-parser"
+#define HHG_PARSER_VER "0.10.0"
+#define HHG_PARSER_VER_MAJOR (0)
+#define HHG_PARSER_VER_MINOR (10)
+#define HHG_PARSER_VER_PATCH (0)
+#define HHG_PARSER_FORMAT_CHAR '%c'
+#define HHG_PARSER_FORMAT_SHORT "%d"
+#define HHG_PARSER_FORMAT_INT "%d"
+#define HHG_PARSER_FORMAT_LONG "%ls"
+#define HHG_PARSER_FORMAT_USHORT "%u"
+#define HHG_PARSER_FORMAT_UINT "%u"
+#define HHG_PARSER_FORMAT_ULONG "%lu"
+#define HHG_PARSER_ARGS_MAX (3)
+
+#endif // HHG_PARSER_CONFIG_H
